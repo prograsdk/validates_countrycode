2c0f2e928aee01d08fe4ec321000b4c438a41a5b
Frederik Spang <frederik@progras.dk>
2018-08-21 16:51:43 +0200

    Initial commit

---
+++ lib/validates_countrycode/version.rb
@@ -0,0 +1,3 @@
+module ValidatesCountrycode
+  VERSION = "0.1.0"
+end
---
+++ spec/validates_countrycode_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe ValidatesCountrycode do
+  it "has a version number" do
+    expect(ValidatesCountrycode::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
